fd18a3613437eb2472b215f170bcd7dd4f6d058a
simantaray <simantaray89@gmail.com>
2022-03-26 22:56:09 +0530

    fixed: BookCards

--- comps/BookCard.js
+++ comps/BookCard.js
@@ -8,7 +8,7 @@ export default function BookCard({ img, index }) {
         <div className="cursor-pointer z-10 relative h-[300px] w-[200px] hover:rotate-[-5deg] hover:scale-110 transition duration-100 ease-out  ">
           <Image alt="book" className="rounded-xl" src={img} layout="fill" />
         </div>
-        <h1 className="z-0 text-green-200 relative ml-[-70px] top-[-350px] text-[200px] pr-[-100px] font-bold">{index}</h1>
+        <h1 className="z-0 text-green-200 relative ml-[-70px] top-[-350px] text-[200px] pr-[-100px] font-bold mb-[-300px]">{index}</h1>
       </div>
     </>
   );
